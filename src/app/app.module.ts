b9c92795d0fe254174faf9e3545f67eb3ae9c156
Gulsen <gulhelince@hotmail.com>
2023-11-05 23:49:01 +0300

    HttpClient Servisi oluşturuldu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { UiModule } from './ui/ui.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { NgxSpinnerModule } from 'ngx-spinner';
+import { HttpClientModule } from '@angular/common/http';
 //import { BaseComponent } from './base/base.component';
 
 
@@ -26,9 +27,12 @@ import { NgxSpinnerModule } from 'ngx-spinner';
     AdminModule,
     UiModule,
     ToastrModule.forRoot(),
-    NgxSpinnerModule
+    NgxSpinnerModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {provide:"baseUrl",useValue:"https://localhost:7295/api",multi:true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
