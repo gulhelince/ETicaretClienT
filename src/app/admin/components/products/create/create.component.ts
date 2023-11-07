369b8d77f448fe6de90468e5f62ed34ef7599849
Gulsen <gulhelince@hotmail.com>
2023-11-07 18:53:08 +0300

    Fluent Validation ile ValidationFilter

--- src/app/admin/components/products/create/create.component.ts
+++ src/app/admin/components/products/create/create.component.ts
@@ -32,11 +32,34 @@ export class CreateComponent extends BaseComponent implements OnInit {
     create_product.stock=parseInt(stock.value);
     create_product.price=parseFloat(price.value);
 
+    if(!name.value){
+      this.alertify.message("Lütfen ürün adını giriniz",{
+        dismissOthers:true,
+        messageType:MessageType.Error,
+        position:Position.TopRight
+      });
+      return;
+    }
+
+    if(parseInt(stock.value) < 0){
+      this.alertify.message("Lütfen stok bilgisini doğru giriniz",{
+        dismissOthers:true,
+        messageType:MessageType.Error,
+        position:Position.TopRight
+      });
+    }
+
     this.productService.create(create_product, () =>{
       this.hideSpinner(SpinnerType.BallAtom);
       this.alertify.message("Ürün başarıyla eklenmiştir.",{
         dismissOthers:true,
         messageType:MessageType.Success,
+        position:Position.TopRight
+      });
+    }, errorMessage =>{
+      this.alertify.message(errorMessage,{
+        dismissOthers:true,
+        messageType:MessageType.Error,
         position:Position.TopRight
       });
     });
